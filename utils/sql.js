a5c91f5a39a91a832adc8262ae72aea1da0baa75
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-12-08 13:17:06 -0500

    fixing

--- utils/sql.js
+++ utils/sql.js
@@ -5,8 +5,13 @@ const connect = sql.createConnection({
     host:config.host,
     password: config.password,
     user: config.user,
-    port:config.port,
     database:config.database
+    
+    // host:config.host,
+    // password: config.password,
+    // user: config.user,
+    // port:config.port,
+    // database:config.database
 })
 
 module.exports = connect;
