ce2e51e8ba4ddded5a496e046501727c89494bf9
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-12-08 13:03:48 -0500

    connect

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 const config = {
-    host: "localhost",
-    port: 3306, // 8889 for mac
-    uname: "root",
-    pword: "", // leave blank for windows
-    database: "db_portfolio"
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user:'b6a7d1368c51cb',
+    password:'03d653cb',
+    database: 'heroku_a934504ae5b64aa'
+
+    // host: 'localhost',
+    // user:'root',
+    // password:'',
+    // database: 'db_portfolio',
+    // port: 3306
 }
 
 module.exports = config;
