4dc4c5bfe30e609a366a4df2f4471de87dea6466
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-12-08 14:37:17 -0500

    sql

--- utils/sql.js
+++ utils/sql.js
@@ -2,10 +2,14 @@ const sql = require('mysql');
 const config = require('../config');
 
 const connect = sql.createConnection({
-    host:config.host,
-    password: config.password,
+    host: config.host,
+    port: config.port,
     user: config.user,
-    database:config.database
+    password: config.password,
+    database: config.database,
+    connectionLimit : 20,
+    queueLimit : 100,
+    waitForConnections : true
     
     // host:config.host,
     // password: config.password,
