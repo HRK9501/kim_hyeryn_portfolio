ddf1ca31b285fbb6c774a9ac73fe36c3efb58ee6
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-12-11 16:27:53 -0500

    host

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
-    // host: 'us-cdbr-iron-east-05.cleardb.net',
-    // user:'b6a7d1368c51cb',
-    // password:'03d653cb',
-    // database: 'heroku_a934504ae5b64aa'
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user:'b6a7d1368c51cb',
+    password:'03d653cb',
+    database: 'heroku_a934504ae5b64aa'
 
-    host: 'localhost',
-    user:'root',
-    password:'',
-    database: 'db_portfolio',
-    port: 3306
+    // host: 'localhost',
+    // user:'root',
+    // password:'',
+    // database: 'db_portfolio',
+    // port: 3306
 }
 
 module.exports = config;
