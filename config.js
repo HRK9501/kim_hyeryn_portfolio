0dbf45b942e644640fca1a9f2ac7fa7fefaeb868
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-12-12 03:53:23 -0500

    host try

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     host: 'us-cdbr-iron-east-05.cleardb.net',
-    user:'b6a7d1368c51cb',
-    password:'03d653cb',
-    database: 'heroku_a934504ae5b64aa'
+    user:'b74cd7a15d064b',
+    password:'74b6e010',
+    database: 'heroku_70a36c4964489fc'
 
     // host: 'localhost',
     // user:'root',
